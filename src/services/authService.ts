dbd5ef1af747e0291d8a19d269c04c291b3c4be6
erlking <sebitsamir@gmail.com>
2026-08-05 15:53:59 +0200

    Adjusted authServices to function properly

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,21 +1,21 @@
+// src/services/authService.ts
 import api from "@/lib/api";
-import { IApiResponse, IUser, ILoginPayload, IRegisterPayload } from "@/types";
+import { IApiResponse, IUser } from "@/types";
 
 export const authService = {
-    // Login
-    login: async (data: ILoginPayload) => {
-        const response = await api.post<IApiResponse<{ accessToken: string, User: IUser }>>(
-            "/auth/login",
-            data
+    register: async (userData: { name: string; email: string; password: string }) => {
+        
+        const response = await api.post<IApiResponse<{ user: IUser; token: string }>>(
+            "/auth/register",
+            userData
         );
         return response.data.data;
     },
 
-    // Register
-    register: async (data: IRegisterPayload) => {
-        const response = await api.post<IApiResponse<{ accessToken: string, user: IUser }>>(
-            "/auth/register",
-            data
+    login: async (credentials: { email: string; password: string }) => {
+        const response = await api.post<IApiResponse<{ user: IUser; token: string }>>(
+            "/auth/login",
+            credentials
         );
         return response.data.data;
     },
